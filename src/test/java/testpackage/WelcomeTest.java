7c9927e7426ab28abe43930bbcc1961f66ad30c7
HrithikDhore <127612381+HrithikDhore@users.noreply.github.com>
2023-04-09 16:40:51 +0530

    Update WelcomeTest.java

--- src/test/java/testpackage/WelcomeTest.java
+++ src/test/java/testpackage/WelcomeTest.java
@@ -4,8 +4,9 @@ public class WelcomeTest {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to git Bash");
-		System.out.println("Hrithik Dhore);
-		System.out.println("Akola);
+		System.out.println("Hrithik Dhore");
+		System.out.println("Akola");
+		System.out.println("Maharashtra");
 
 	}
 
